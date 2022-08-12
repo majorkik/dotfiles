56b0494a801fb7ef3a3d31a892596806a898adbf
Rodion Belovitskiy <belovickiy.rodion@gmail.com>
2022-08-12 15:12:12 +0000

    feat: add custom config for nvim

---
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -0,0 +1,31 @@
+-- custom.plugins.lspconfig
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "svlangserver", "hls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig["rust_analyzer"].setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        loadOutDirsFromCheck = true,
+      },
+      procMacro = {
+        enable = true,
+      },
+      checkOnSave = {
+        command = "clippy",
+      },
+    },
+  },
+}
